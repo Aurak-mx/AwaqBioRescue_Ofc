a92db4c4f343903847a81277d1686edf5d50020b
Zumva <mguillex3@gmail.com>
2026-03-23 15:39:23 -0600

    Nuevo Repositorio

---
+++ Assets/_Project/Minigames/MinigameMarcelo/Scripts/LauncherPro.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class LauncherPro : MonoBehaviour
+{
+    public void OpenGame5Menu()
+    {
+        SceneManager.LoadScene("MainMenu"); 
+    }
+    
+}
